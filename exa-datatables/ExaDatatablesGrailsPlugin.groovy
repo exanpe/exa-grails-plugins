80d5e5b44ea45ef0ad81c5ccbfb7869de06af6c9
lguerin <laurent@dreamcat.fr>
2015-08-01 17:09:03 +0200

    Bump version

--- exa-datatables/ExaDatatablesGrailsPlugin.groovy
+++ exa-datatables/ExaDatatablesGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ExaDatatablesGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
