b31c12127bde542981efbf5d3cf7c94ab7a8a85e
Laurent Guérin <lguerin.fr@gmail.com>
2016-06-05 21:07:01 +0200

    Add some tests

--- exa-datatables/test/unit/fr/exanpe/grails/datatables/util/TagLibUtilsSpec.groovy
+++ exa-datatables/test/unit/fr/exanpe/grails/datatables/util/TagLibUtilsSpec.groovy
@@ -1,9 +1,14 @@
 package fr.exanpe.grails.datatables.util
 
+import fr.exanpe.grails.datatables.test.Person
+import grails.converters.JSON
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class TagLibUtilsSpec extends Specification {
+
+    Person person1 = new Person(name: "Laurent", address: "10 rue de Paris", zipCode: "75001", city: "Paris", age: 35)
+    String jsonPerson = "{name: \"Laurent\", address: \"10 rue de Paris\", zipCode: \"75001\", city: \"Paris\", age: 35}"
 
     @Unroll
     def 'Convert "#input" string column names as List'() {
@@ -16,5 +21,21 @@ class TagLibUtilsSpec extends Specification {
         ["name", "age"]             | "name age"
         ["name", "zipCode"]         | " name,  zipCode  "
         []                          | ""
+    }
+
+    def 'Build Datatable model row from input data'() {
+        when: 'Build from an object instance'
+        def datatableModelRow = TagLibUtils.addDatatableModelRow(person1, ['name', 'zipCode'])
+
+        then:
+        datatableModelRow.cells.get('name').value == "Laurent"
+        !datatableModelRow.cells.containsKey('age')
+
+        when: 'Build from JSON object'
+        datatableModelRow = TagLibUtils.addDatatableModelRow(JSON.parse(jsonPerson), ['name', 'zipCode'])
+
+        then:
+        datatableModelRow.cells.get('name').value == "Laurent"
+        !datatableModelRow.cells.containsKey('age')
     }
 }
